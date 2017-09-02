22b8ddd556c50c6ac8081199850c0ba4de76a120
Chen Chen <anranip@126.com>
2017-09-02 13:18:13 +0800

    add Bridge

---
+++ 单一职责模式/Bridge/bridge1.cpp
@@ -0,0 +1,144 @@
+class Messager{
+public:
+    virtual void Login(string username, string password)=0;
+    virtual void SendMessage(string message)=0;
+    virtual void SendPicture(Image image)=0;
+
+    virtual void PlaySound()=0;
+    virtual void DrawShape()=0;
+    virtual void WriteText()=0;
+    virtual void Connect()=0;
+    
+    virtual ~Messager(){}
+};
+
+//平台实现
+class PCMessagerBase : public Messager{
+public:   
+    virtual void PlaySound(){
+        //**********
+    }
+    virtual void DrawShape(){
+        //**********
+    }
+    virtual void WriteText(){
+        //**********
+    }
+    virtual void Connect(){
+        //**********
+    }
+};
+
+class MobileMessagerBase : public Messager{
+public:
+    virtual void PlaySound(){
+        //==========
+    }
+    virtual void DrawShape(){
+        //==========
+    }
+    virtual void WriteText(){
+        //==========
+    }
+    virtual void Connect(){
+        //==========
+    }
+};
+
+//业务抽象
+class PCMessagerLite : public PCMessagerBase {
+public:
+    
+    virtual void Login(string username, string password){
+        
+        PCMessagerBase::Connect();
+        //........
+    }
+    virtual void SendMessage(string message){
+        
+        PCMessagerBase::WriteText();
+        //........
+    }
+    virtual void SendPicture(Image image){
+        
+        PCMessagerBase::DrawShape();
+        //........
+    }
+};
+
+class PCMessagerPerfect : public PCMessagerBase {
+public:
+    
+    virtual void Login(string username, string password){
+        
+        PCMessagerBase::PlaySound();
+        //********
+        PCMessagerBase::Connect();
+        //........
+    }
+    virtual void SendMessage(string message){
+        
+        PCMessagerBase::PlaySound();
+        //********
+        PCMessagerBase::WriteText();
+        //........
+    }
+    virtual void SendPicture(Image image){
+        
+        PCMessagerBase::PlaySound();
+        //********
+        PCMessagerBase::DrawShape();
+        //........
+    }
+};
+
+class MobileMessagerLite : public MobileMessagerBase {
+public:
+    
+    virtual void Login(string username, string password){
+        
+        MobileMessagerBase::Connect();
+        //........
+    }
+    virtual void SendMessage(string message){
+        
+        MobileMessagerBase::WriteText();
+        //........
+    }
+    virtual void SendPicture(Image image){
+        
+        MobileMessagerBase::DrawShape();
+        //........
+    }
+};
+
+class MobileMessagerPerfect : public MobileMessagerBase {
+public:
+    
+    virtual void Login(string username, string password){
+        
+        MobileMessagerBase::PlaySound();
+        //********
+        MobileMessagerBase::Connect();
+        //........
+    }
+    virtual void SendMessage(string message){
+        
+        MobileMessagerBase::PlaySound();
+        //********
+        MobileMessagerBase::WriteText();
+        //........
+    }
+    virtual void SendPicture(Image image){
+        
+        MobileMessagerBase::PlaySound();
+        //********
+        MobileMessagerBase::DrawShape();
+        //........
+    }
+};
+
+void Process(){
+	//编译时装配
+	Messager *m = new MobileMessagerPerfect();
+}
---
+++ 单一职责模式/Bridge/bridge2.cpp
@@ -0,0 +1,103 @@
+class Messager{
+protected:
+	MessagerImp* messagerImp;
+public:
+	virtual void Login(string username, string password) = 0;
+	virtual void SendMessage(string message) = 0;
+	virtual void SendPicture(Image image) = 0;
+	virtual ~Messager(){}
+};
+
+class MessagerImp{
+public:
+	virtual void PlaySound() = 0;
+	virtual void DrawShape() = 0;
+	virtual void WriteText() = 0;
+	virtual void Connect() = 0;
+	virtual MessagerImp(){}
+};
+
+//平台实现
+class PCMessagerImp : public MessagerImp{
+public:
+	virtual void PlaySound(){
+		//*****
+	}
+	virtual void DrawShape(){
+		//*****
+	}
+	virtual void WriteText(){
+        //**********
+    }
+    virtual void Connect(){
+        //**********
+    }
+};
+
+class MobileMessagerImp : public MessagerImp{
+public:
+
+    virtual void PlaySound(){
+        //==========
+    }
+    virtual void DrawShape(){
+        //==========
+    }
+    virtual void WriteText(){
+        //==========
+    }
+    virtual void Connect(){
+        //==========
+    }
+};
+
+//业务抽象
+class MessagerLite : public Messager{
+public:
+    virtual void Login(string username, string password){
+        
+        messagerImp->Connect();
+        //........
+    }
+    virtual void SendMessage(string message){
+        
+        messagerImp->WriteText();
+        //........
+    }
+    virtual void SendPicture(Image image){
+        
+        messagerImp->DrawShape();
+        //........
+    }
+};
+
+class MessagerPerfect : public Messager{
+public:
+    virtual void Login(string username, string password){
+        
+        messagerImp->PlaySound();
+        //********
+        messagerImp->Connect();
+        //........
+    }
+    virtual void SendMessage(string message){
+        
+        messagerImp->PlaySound();
+        //********
+        messagerImp->WriteText();
+        //........
+    }
+    virtual void SendPicture(Image image){
+        
+        messagerImp->PlaySound();
+        //********
+        messagerImp->DrawShape();
+        //........
+    }
+};
+
+void Process(){
+	//运行时装配
+	MessagerImp* mImp = new PCMessagerImp();
+	Messager* m = new MessagerPerfect(mImp);
+}
