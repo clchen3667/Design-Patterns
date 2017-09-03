0d5604c5c6955a9e3990dd36996a4774b58e4ad7
Chen Chen <anranip@126.com>
2017-09-03 17:04:57 +0800

    add Flyweight Method

---
+++ 对象性能模式/Flyweight/flyweight.cpp
@@ -0,0 +1,39 @@
+class Font {
+private:
+
+    //unique object key
+    string key;
+    
+    //object state
+    //....
+    
+public:
+    Font(const string& key){
+        //...
+    }
+};
+
+class FontFactory{
+private:
+    map<string,Font*> fontPool;
+
+public:
+    Font* GetFont(const string& key){
+
+        map<string,Font*>::iterator item = fontPool.find(key);
+
+        if(item != footPool.end()){
+            return fontPool[key];
+        }
+        else{
+            Font* font = new Font(key);
+            fontPool[key] = font;
+            return font;
+        }
+
+    }
+
+    void clear(){
+        //...
+    }
+};
